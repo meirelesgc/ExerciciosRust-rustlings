022bfe82b2fc429ae7f05d76dccb0c7afadea739
meirelesgc <geu_docsta@outlook.com>
2026-02-10 14:43:24 -0300

    10/02/2026

---
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn slice_out_of_array() {
+        let a = [1, 2, 3, 4, 5];
+
+        nice_slice = []
+
+        assert_eq!([2, 3, 4], nice_slice);
+    }
+}
